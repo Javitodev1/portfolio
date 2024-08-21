d38cb92d5d3a870620e43ceda57da5b651f7c6a6
Javitodev <javito.dev@gmail.com>
2024-08-21 19:37:24 -0400

    refactoring....

---
+++ src/home-page/modules/index.ts
@@ -0,0 +1,6 @@
+import Hero from './Hero.astro'
+import Experience from './Experience.astro'
+import Projects from './Projects.astro'
+import About from './About.astro'
+
+export {Hero, Experience, Projects, About}
